a871d1cbad3d5af3aa092aef4541199bd407d4cc
kushtarbek <dr.kushtarbek@gmail.com>
2023-12-11 18:04:09 -0800

    added code formatter

--- ecom-app/src/components/ProductCards.js
+++ ecom-app/src/components/ProductCards.js
@@ -5,7 +5,7 @@ import { RestLink } from "apollo-link-rest";
 
 
 export default function ProductCards() {
-    const httpLink = new HttpLink({
+    const httpLink = new httpLink({
         uri: "https://48p1r2roz4.sse.codesandbox.io",
       });
       
