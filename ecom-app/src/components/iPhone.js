599db2436c444ebab5aec4f136e93235008b36b9
kushtarbek <dr.kushtarbek@gmail.com>
2023-12-13 23:27:05 -0800

    added figure image

--- ecom-app/src/components/iPhone.js
+++ ecom-app/src/components/iPhone.js
@@ -53,10 +53,25 @@ import { TbDeviceAirpods } from "react-icons/tb";
                 <h6>AirPods Pro</h6>
                 </li> 
             </div>
-            <div><p>
+            <div>
+                <p>
             Get $200–$650 in credit toward iPhone 15 or iPhone 15 Pro when you trade in iPhone 11 or higher.1 Shop iPhone
-                </p></div>
+                </p>
+            </div>
         </nav>
+        <div style={{backgroundColor:'black'}}>
+            <div >
+                <h4 style={{color: 'white'}}>iPhone 15 Pro </h4>
+                <h3 style={{color: 'orange'}}>Titanium</h3>
+                <h5 style={{color: 'white'}}>from $999 or $41.62/mo for 24mo.</h5>
+                <button>Buy</button>
+                <h4 style={{color: 'white'}}><a>Learn more</a></h4>
+                <figure>
+                <img src="https://www.telstra.com.au/content/dam/tcom/library/cam-campaign/CAM-DCAEG57359-apple-iPhone15Pro-preorder-campaignEdgeDesktop-1920x500.png" alt="iphone" style={{width:'100%'}}/>
+
+                </figure>
+            </div>
+        </div>
     </div>
   )
 }
