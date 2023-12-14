e2f04a79bc403fe9a4515004c32cd8799c00ced0
kushtarbek <dr.kushtarbek@gmail.com>
2023-12-13 23:13:47 -0800

    iphones page added

---
+++ ecom-app/src/components/iPhone.js
@@ -0,0 +1,64 @@
+import React from 'react'
+import { SlScreenSmartphone } from "react-icons/sl";
+import { TbDeviceAirpods } from "react-icons/tb";
+
+
+ function IPhone() {
+  return (
+    <div>
+        <nav>
+            <div>
+                <li>
+                <SlScreenSmartphone />
+                <h6>iPhone 15 Pro</h6>
+                </li>
+                <li>
+                <TbDeviceAirpods />
+                <h6>AirPods Pro</h6>
+                </li> 
+                <li>
+                <TbDeviceAirpods />
+                <h6>AirPods Pro</h6>
+                </li> 
+                <li>
+                <TbDeviceAirpods />
+                <h6>AirPods Pro</h6>
+                </li> 
+                <li>
+                <TbDeviceAirpods />
+                <h6>AirPods Pro</h6>
+                </li> 
+                <li>
+                <TbDeviceAirpods />
+                <h6>AirPods Pro</h6>
+                </li> 
+                <li>
+                <TbDeviceAirpods />
+                <h6>AirPods Pro</h6>
+                </li> 
+                <li>
+                <TbDeviceAirpods />
+                <h6>AirPods Pro</h6>
+                </li> 
+                <li>
+                <TbDeviceAirpods />
+                <h6>AirPods Pro</h6>
+                </li> 
+                <li>
+                <TbDeviceAirpods />
+                <h6>AirPods Pro</h6>
+                </li> 
+                <li>
+                <TbDeviceAirpods />
+                <h6>AirPods Pro</h6>
+                </li> 
+            </div>
+            <div><p>
+            Get $200–$650 in credit toward iPhone 15 or iPhone 15 Pro when you trade in iPhone 11 or higher.1 Shop iPhone
+                </p></div>
+        </nav>
+    </div>
+  )
+}
+
+export default IPhone;
